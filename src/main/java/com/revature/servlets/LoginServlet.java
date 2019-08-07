90a5fd8264570a36829c6b844ee9076cc58be2b8
Leoschenck <leoschenck@gmx.de>
2019-08-07 19:49:36 -0400

    some changeeees

--- src/main/java/com/revature/servlets/LoginServlet.java
+++ src/main/java/com/revature/servlets/LoginServlet.java
@@ -22,30 +22,32 @@ public class LoginServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("In login servlet..." + req.getParameter("username") + "  ||  "+ req.getParameter("password"));
+		PrintWriter pw = resp.getWriter();
 		try {
 			// Retrieves any employees who match the username/password
 			UserDaoImpl udi = new UserDaoImpl();
 			int userId = udi.loginUser(req.getParameter("username"), req.getParameter("password"));
-			resp.setContentType("text/html");
+			String uName = req.getParameter("username");
+			String password = req.getParameter("password");
 
-			// Sends the user to the home screen if the username and password match an
-			// user
+			// Sends the user to the home screen if the username and password match a user
+			// or tells him there is no match and directs him back to the login.
 			if (userId >= 0) {
-				HttpSession ses = req.getSession(true);
+				HttpSession ses = req.getSession();
 				ses.setAttribute("userId", userId);
+				ses.setAttribute("uName", uName);
+				ses.setAttribute("password", password);
 				System.out.println("User logged in! Id = " + userId);
 				resp.sendRedirect("/TRMS/home");
 			}
-			// Lets the user know the username and password don't match any user records
 			else {
-				PrintWriter pw = resp.getWriter();
 				resp.setContentType("text/html");
 				pw.println("<script type=\"text/javascript\">");
 				pw.println("alert('Username and password do not match!');");
 				pw.println("</script>");
-				req.getRequestDispatcher("/login.html").include(req, resp);
-				pw.close();
+				req.getRequestDispatcher("/login.html").forward(req, resp);
 			}
+			pw.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
