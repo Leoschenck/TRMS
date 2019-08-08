dce659e4ee2605876fd9cff71a5debfd3e1c99b9
Justin <jhua8837@gmail.com>
2019-08-08 14:00:29 -0400

    html stuff

---
+++ src/main/webapp/approval.js
@@ -0,0 +1,6 @@
+var result = document.getElementByID("cell");
+result.addEventListener("click", modal)
+function modal(){
+	var leftTable = document.getElementByID("lefttable");
+	console.log("here");
+}
