93dec36ff743e91fc2a87d4b6b5b757a1baa9aa6
Justin <jhua8837@gmail.com>
2019-08-08 21:13:15 -0400

    html

--- src/main/webapp/approval.js
+++ src/main/webapp/approval.js
@@ -1,6 +1,37 @@
-var result = document.getElementByID("cell");
-result.addEventListener("click", modal)
-function modal(){
-	var leftTable = document.getElementByID("lefttable");
-	console.log("here");
+// var result = document.getElementByID("cell");
+// result.addEventListener("click", modal)
+// function modal(){
+// 	var leftTable = document.getElementByID("lefttable");
+// 	console.log("here");
+// }
+
+function loadForms(userId){
+	// console.log("in loadUserHome");
+    // if(userId != null){
+    //     document.getElementById("userId").innerHTML = userId.id;
+    // } else {
+    //     document.getElementById("vgName").innerHTML = "There is no record associated with that ID";
+    // }
+}
+
+function getForms() {
+    console.log("In get User function!");
+    var xhr = new XMLHttpRequest();
+    var forms = '';
+    xhr.onreadystatechange = function () {
+        console.log("In ORSC " + xhr.readyState + xhr.status);
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log(xhr.responseText);
+            forms = JSON.parse(xhr.responseText);
+            loadForms(forms);
+            console.log(forms);
+        }
+    }
+    xhr.open("GET", "http://localhost:8080/TRMS/user", true);
+    xhr.send();
+}
+
+window.onload = function(){
+	var curUser = getForms();
+    console.log(curUser)
 }
