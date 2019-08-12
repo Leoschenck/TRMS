8b4031517046e4e174e64c6feabd136b46184e93
ljapplebaum <ljapplebaum@gmail.com>
2019-08-11 22:44:30 -0400

    form fixed

--- src/main/webapp/form.js
+++ src/main/webapp/form.js
@@ -2,13 +2,16 @@ var today = new Date();
 var dd = today.getDate();
 var mm = today.getMonth() + 1; // January is 0
 var yyyy = today.getFullYear();
+var ckObj = {};
+var ball;
+var deptsObj = [];
 
 if (dd < 10) {
-	dd = '0' + dd
+	dd = '0' + dd;
 }
 
 if (mm < 10) {
-	mm = '0' + mm
+	mm = '0' + mm;
 }
 
 today = yyyy + '-' + mm + '-' + dd;
@@ -38,9 +41,20 @@ today = yyyy + '-' + mm + '-' + dd;
 // console.log('Days since ' + Jan1st2010.toLocaleDateString() + ': '
 // + Date.daysBetween(Jan1st2010, today));
 window.onload = function () {
-    handleOptions(data);
+    var ck = decodeURIComponent(document.cookie);
+    var cks = ck.split(';');
+    for (var i = 0; i < cks.length; i++) {
+        var tempCk = cks[i].split('=');
+        tempCk[0] = tempCk[0].trim();
+        tempCk[1] = tempCk[1].trim();
+        ckObj[tempCk[0]] = tempCk[1];
+        console.log(tempCk[0]);
+        console.log(ckObj[tempCk[0]]);
+    }
+    var data = 0;
     document.getElementById("cost").addEventListener("change", alerter);
     document.getElementById("typeOfEvent").addEventListener("change", alerter);
+    handleOptions(data);
 //document.getElementById("formSubmit").addEventListener("click", postForm,
 //		false);
 //document.getElementById("currentDate").value = today;
@@ -83,19 +97,50 @@ function jsonBuiilder() {
     return json;
 };*/
 
-handleOptions(data){
-    var ck = decodeURIComponent(document.cookie);
-    var cks = ck.split(';');
-    var ckObj = {};
-    for (var i = 0; i < cks.length; i++) {
-        var tempCk = cks[i].split('=');
-        tempCk[0] = tempCk[0].trim();
-        tempCk[1] = tempCk[1].trim();
-        ckObj[tempCk[0]] = tempCk[1];
-        console.log(tempCk[0]);
-        console.log(ckObj[tempCk[0]]);
+function inputDepts(depts) {
+    var allIds = Object.keys(depts);
+    var deptName = document.getElementById("deptId");
+    var opt = document.createElement("option");
+    opt.value = 0;
+    opt.selected = "selected";
+    opt.text = "Select Department";
+    deptName.appendChild(opt);
+    for (var i = 0; i < allIds.length; i++) {
+        var opt = document.createElement("option");
+        opt.value = allIds[i];
+        opt.text = depts[opt.value];
+        console.log(opt.value);
+        console.log(opt.text);
+        deptName.appendChild(opt);
     }
-    document.getElementById("cost").max = ckObj[""];
+
+}
+
+function handleOptions(data){
+        var xhr = new XMLHttpRequest();
+        var depts = '';
+        xhr.onreadystatechange = function () {
+            console.log("In ORSC " + xhr.readyState + xhr.status);
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                console.log(xhr.responseText);
+                depts = JSON.parse(xhr.responseText);
+                //var allDepts = xhr.responseTest + "";
+                //console.log(allDepts);
+                //allDepts = allDepts.split(',');
+                //var tempDept;
+                //var tempObj = {};
+                //for (var i = 0; i < allDepts.length; i++) {
+                //    tempDept = allDepts[i].split(':');
+                //    tempObj.id = tempDept[0].substring(1, tempDept.length - 1)
+                //    tempObj.name = tempDept[1].substring(1, tempDept[1].length - 1);
+                //    deptsObj[i] = tempObj;
+                //}
+               // console.log();
+                inputDepts(depts);
+            }
+        }
+        xhr.open("GET", "http://localhost:8080/TRMS/getdepartments", true);
+        xhr.send();
 };
 
 function alerter() {
@@ -104,47 +149,46 @@ function alerter() {
 	var amount = document.getElementById("cost").value;
 
 	var event = document.getElementById("typeOfEvent").value;
-
+	var factor = 0;
 	switch (event) {
 	case "1":
-		document.getElementById("reimbursementamount").value = parseFloat(
-				(amount * .80)).toFixed(2);
+		factor = 0.80;
 		//document.getElementById("reimbamtEst").value = parseFloat(
 		//		(amount )).toFixed(2);
 		;
 		break;
 	case "2":
-		document.getElementById("reimbursementamount").value = parseFloat(
-				(amount * .60)).toFixed(2);
+		factor = 0.60;
 		//document.getElementById("reimbamtEst").value = parseFloat(
 		//		(amount * .60)).toFixed(2);
 		break;
 	case "3":
-		document.getElementById("reimbursementamount").value = parseFloat(
-				(amount * .75)).toFixed(2);
+		factor = 0.75;
 		//document.getElementById("reimbamtEst").value = parseFloat(
 		//		(amount * .80)).toFixed(2);
 		break;
 	case "4":
-		document.getElementById("reimbursementamount").value = parseFloat(
-				amount ).toFixed(2);
+		factor =1.00;
 		//document.getElementById("reimbamtEst").value = parseFloat(
 		//		amount * .75)
 		//		.toFixed(2);
 		break;
 	case "5":
-		document.getElementById("reimbursementamount").value = parseFloat(
-				(amount * .90)).toFixed(2);
+		factor = 0.90;
 		//document.getElementById("reimbamtEst").value = parseFloat(
 		//		(amount * .90)).toFixed(2);
 		break;
 	case "6":
-		document.getElementById("reimbursementamount").value = parseFloat(
-				(amount * .30)).toFixed(2);
+		factor = 0.30;
 		//document.getElementById("reimbamtEst").value = parseFloat(
 		//		(amount * .30)).toFixed(2);
 		break;
 	default:
 		console.log("Reimbursement Amount Error");
 	}
+	document.getElementById("reimbursementamount").value = parseFloat(
+			(amount * factor)).toFixed(2);
+        ball = ckObj["remainingReimbursementAmount"].toString();
+        document.getElementById("cost").max = (ball / factor);
+	
 }
