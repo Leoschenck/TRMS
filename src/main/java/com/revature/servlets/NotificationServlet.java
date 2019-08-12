b1a463ab7007354c2f871416603bf1651da8a6cc
Leoschenck <leoschenck@gmx.de>
2019-08-12 01:49:28 -0400

    added needed servlet

---
+++ src/main/java/com/revature/servlets/NotificationServlet.java
@@ -0,0 +1,38 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class NotificationServlet
+ */
+public class NotificationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession s = request.getSession(false);
+		if(s != null) {
+			request.getRequestDispatcher("/notifications.html");
+		} else {
+			response.sendRedirect("/TRMS/login");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
